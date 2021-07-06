dc37a16b2c293277b5238062013e4f57a3be18c9
Sarita <dsarritta@gmail.com>
2021-07-06 18:07:00 +0545

    redux reducer: add reducer

---
+++ src/reducers/reducer.js
@@ -0,0 +1,39 @@
+import * as actions from "../constants/ActionTypes";
+import { browserHistory } from "react-router";
+
+const initialState = {
+  isSignedIn: false,
+  userData: null,
+  searchInput: null,
+  blogs:[],
+  increment: 0,
+
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case actions.SIGN_IN:
+      return { ...state, isSignedIn: true };
+
+    case actions.SIGN_OUT:
+      return {
+        ...state,
+        isSignedIn: false,
+      };
+
+    case actions.USER_DATA:
+      return { ...state, userData: action.payload };
+
+    case actions.FETCH_BLOGS:
+      return { ...state, blogs: action.payload };
+
+    case actions.ADD_NEW_POST:
+      return { ...state, blogs: [...state.blogs, action.payload] };
+
+    case actions.INCREMENT:
+      return { increment: state.increment + 1 };
+
+    default:
+      return state;
+  }
+};
