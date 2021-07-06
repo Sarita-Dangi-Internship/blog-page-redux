98f53dd32ac360e81d4774134862554b87a44489
Sarita <dsarritta@gmail.com>
2021-07-06 18:04:46 +0545

    constants: add constants for action types

---
+++ src/constants/ActionTypes.js
@@ -0,0 +1,8 @@
+export const SIGN_IN = "SIGN_IN";
+export const SIGN_OUT = "SIGN_OUT";
+export const USER_DATA = "USER_DATA";
+export const FETCH_BLOGS = "FETCH_BLOGS";
+export const POST_LOGIN = "POST_LOGIN";
+export const ADD_NEW_POST = "ADD_NEW_POST";
+
+export const INCREMENT = "INCREMENT";
