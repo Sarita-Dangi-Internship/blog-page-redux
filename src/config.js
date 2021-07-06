00041ffd3429a768de93f0f7bbdd3ae1f39eced0
Sarita <dsarritta@gmail.com>
2021-07-06 18:15:56 +0545

    config file: add bse url andendpoints

---
+++ src/config.js
@@ -0,0 +1,12 @@
+const config = {
+  api: {
+    endpoints: {
+      posts: "posts",
+      login:"auth/login",
+      subcomments: "api/comments/",
+    },
+  },
+  baseURI: "http://52.43.200.182:8000/api/",
+};
+
+export default config;
