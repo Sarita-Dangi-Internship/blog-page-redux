07d1f02b3fa7f23a3403725340378ddda5745f40
Sarita <dsarritta@gmail.com>
2021-07-06 14:52:08 +0545

    homepage component: call add new post service layer and handle on save, cancel and change input

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,16 +1,69 @@
 import React, { Component } from "react";
 import NavBar from "./NavBar";
+import { getAccessToken } from "../utils/token";
+import * as blogService from "../services/blogs";
 import { connect } from "react-redux";
-import { fetchBlogs, searchBlog } from "../actions/actions";
+import { fetchBlogs, searchBlog, addNewPost } from "../actions/actions";
 
 class HomePage extends Component {
   state = {
+    isCreatePost: false,
     search: "",
+    title: "",
+    description: "",
+    id: "",
   };
 
   componentDidMount() {
     this.props.fetchBlogs();
   }
+
+  handleOnEdit = () => {
+    this.setState({ isCreatePost: true });
+  };
+
+  handleOnChange = (event) => {
+    this.setState({ [event.target.id]: event.target.value });
+    console.log("input", event.target.value);
+  };
+
+
+  handleOnSave = async () => {
+    // this.props
+    //   .addNewPost(this.state.title, this.state.description)
+    //   .then((data) => {
+    //     this.setState({
+    //       title: data.title,
+    //       description: data.description,
+    //       // users: { id: this.props.userData.id},
+    //     });
+    //     console.log("data", data);
+    //   })
+    //   .catch((error) => console.log(error));
+
+    const data = {
+      title: this.state.title,
+      description: this.state.description,
+      users: { _id: this.props.userData.id },
+    };
+
+    try {
+      const response = await blogService.addNewPost(data);
+      console.log(response);
+
+      this.setState({
+        title: "",
+        description: "",
+        isCreatePost: false,
+      });
+    } catch (error) {
+      console.log("error");
+    }
+  };
+
+  handleOnCancel = () => {
+    this.setState({ isCreatePost: false });
+  };
 
   /**
    * function to handle search input
@@ -19,20 +72,41 @@ class HomePage extends Component {
     this.setState({
       search: event.target.value,
     });
-    
+
     this.props.searchBlog(this.state.search);
   };
 
   render() {
+    const { isCreatePost, title, description } = this.state;
     return (
       <>
         <NavBar />
         <div className="container">
-          <div className="create-blog">
-            <button>
-              <i className="fas fa-plus"></i>Create New Blog
-            </button>
-          </div>
+          {isCreatePost ? (
+            <div className="blog-post">
+              <label htmlFor="title">Title</label>
+              <input id="title" value={title} onChange={this.handleOnChange} />
+              <label htmlFor="description">Description</label>
+              <input
+                id="description"
+                value={description}
+                onChange={this.handleOnChange}
+              />
+              <button className="button" onClick={this.handleOnSave}>
+                Save
+              </button>
+              <button className="button" onClick={() => this.handleOnCancel()}>
+                Cancel
+              </button>
+            </div>
+          ) : (
+            <div className="create-blog">
+              <button onClick={() => this.handleOnEdit()}>
+                <i className="fas fa-plus"></i>Create New Blog
+              </button>
+            </div>
+          )}
+
           <div className="search">
             <input
               type="search"
@@ -81,7 +155,10 @@ class HomePage extends Component {
 const mapStateToProps = (state) => {
   return {
     blogs: state.auth.blogs,
+    userData: state.auth.userData,
   };
 };
 
-export default connect(mapStateToProps, { fetchBlogs, searchBlog })(HomePage);
+export default connect(mapStateToProps, { fetchBlogs, searchBlog, addNewPost })(
+  HomePage
+);
